38cc12d9ed153740a5227adb8953146163799bc4
Lone Nilsson <onlylonely1986@gmail.com>
2019-09-13 13:12:39 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace _1dv607_a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\SaveData\members.txt";
+            Console.WriteLine(File.Exists(filePath) ? "File exists." : "File does not exist.");
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            DateTime dateTimeNow = DateTime.Now;
+            
+            lines.Add($"Sven, Olsson, 691202, {dateTimeNow}");
+            File.WriteAllLines(filePath, lines);
+
+
+            // Console.ReadLine();
+        }
+    }
+}
